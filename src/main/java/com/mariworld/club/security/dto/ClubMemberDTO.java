2eacc2c50807dfd10dd820834b85b77752a2465b
MARI <mari20201101@gmail.com>
2021-06-01 10:20:41 +0900

    Upgrade OAuth2 login service

--- src/main/java/com/mariworld/club/security/dto/ClubMemberDTO.java
+++ src/main/java/com/mariworld/club/security/dto/ClubMemberDTO.java
@@ -3,15 +3,19 @@ package com.mariworld.club.security.dto;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 
 @Getter
 @Setter
-public class ClubMemberDTO extends User {
+@ToString
+public class ClubMemberDTO extends User implements OAuth2User {
     private String email;
     private String name;
     private boolean fromSocial;
+    private Map<String, Object> attr;
 
     //부모 생성자 + 내 생성자.
     //password는 부모가 이미 가지고 있는 멤버변수 = 내거!
@@ -22,5 +26,18 @@ public class ClubMemberDTO extends User {
         super(username, password, authorities);
         this.email = username;
         this.fromSocial = fromSocial;
+    }
+
+    public ClubMemberDTO(String email
+            , String name
+            , boolean fromSocial
+            , Collection<? extends GrantedAuthority> authorities
+            , Map<String, Object> attr){
+        this(email, name, fromSocial,authorities);//위에 작성한 생성자
+        this.attr = attr;
+    }
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attr;
     }
 }
---
+++ src/main/java/com/mariworld/club/security/service/ClubOAuth2UserDetailsService.java
@@ -0,0 +1,73 @@
+package com.mariworld.club.security.service;
+
+import com.mariworld.club.entity.ClubMember;
+import com.mariworld.club.entity.ClubMemberRole;
+import com.mariworld.club.repository.ClubMemberRepository;
+import com.mariworld.club.security.dto.ClubMemberDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ClubOAuth2UserDetailsService extends DefaultOAuth2UserService {
+
+    private final ClubMemberRepository clubMemberRepository;
+    private final PasswordEncoder passwordEncoder;
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+      OAuth2User oAuth2User  = super.loadUser(userRequest);
+      oAuth2User.getAuthorities().forEach(System.out::println);
+      oAuth2User.getAttributes().forEach((k,v)->log.info(k+" : "+ v));
+
+      String clientName  = userRequest.getClientRegistration().getClientName();
+      String email = "";
+
+      if(clientName.equals("Google")){
+          email = oAuth2User.getAttribute("email");
+      }
+      ClubMember clubMember = saveSocialMember(email);
+
+      ClubMemberDTO clubMemberDTO = new ClubMemberDTO(
+              email
+              , clubMember.getPassword()
+              ,true
+              , clubMember.getRoleSet()
+                .stream()
+                .map(r->new SimpleGrantedAuthority("ROLE_"+r.name()))
+                .collect(Collectors.toSet())
+              , oAuth2User.getAttributes()
+      );
+      clubMemberDTO.setName(clubMember.getName());
+
+        return clubMemberDTO;
+    }
+
+    private ClubMember saveSocialMember(String email) {
+        Optional<ClubMember> result
+                = clubMemberRepository.findByEmail(email, true);
+        if(result.isPresent()) return result.get(); //이미 가입이력이 있는 회원. DB에 저장되어 있다
+        ClubMember clubMember = ClubMember.builder() //그렇지 않으면 - 회원가입 시킨다
+                .email(email)
+                .name(email)
+                .password(passwordEncoder.encode("1111"))
+                .fromSocial(true)
+                .build();
+        clubMember.addClubMemberRole(ClubMemberRole.USER);
+        clubMemberRepository.save(clubMember);
+        return clubMember;
+    }
+}
