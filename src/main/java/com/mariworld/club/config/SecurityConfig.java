16f76c89431701350bdf28f922314aa29eda8610
MARI <mari20201101@gmail.com>
2021-07-23 17:03:12 +0900

    Add api login filter

--- src/main/java/com/mariworld/club/config/SecurityConfig.java
+++ src/main/java/com/mariworld/club/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.mariworld.club.config;
 
+import com.mariworld.club.security.filter.ApiCheckFilter;
+import com.mariworld.club.security.filter.ApiLoginFilter;
 import com.mariworld.club.security.handler.ClubLoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +10,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public ApiCheckFilter apiCheckFilter(){
+        return new ApiCheckFilter("/notes/**/*");
+    }
+
+    @Bean
+    public ApiLoginFilter apiLoginFilter() throws Exception {
+        ApiLoginFilter apiLoginFilter = new ApiLoginFilter("/api/login");
+        apiLoginFilter.setAuthenticationManager(authenticationManager());
+        return apiLoginFilter;
+    }
 
     @Bean
     public ClubLoginSuccessHandler clubLoginSuccessHandler() {
@@ -28,14 +43,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/sample/admin").hasRole("ADMIN")
                 .antMatchers("/sample/**").permitAll()
                 .and()*/
-        http
-                .formLogin()
-                .and()
-                .rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailsService())
-                .and()
-                .csrf().disable()
-                .oauth2Login().successHandler(clubLoginSuccessHandler())
-                .and()
-                .logout();
+        http.formLogin();
+        http.rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailsService());
+        http.csrf().disable();
+        http.oauth2Login().successHandler(clubLoginSuccessHandler());
+        http.logout();
+
+        http.addFilterBefore(apiCheckFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
